2e57dc861df916eac7cfe5d8bccc06ab757c3058
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-20 17:38:54 +0100

    style: update Footer component with new DC lists and enhance layout with social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,46 +5,55 @@ export default function Footer() {
                 <div className="dc-lists">
                     <div className="first-list">
                         <ul>DC COMICS
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
                         </ul>
                         <ul>SHOP
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
                         </ul>
                     </div>
                     <div className="second-list">
                         <ul>DC
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
+                            <li>Terms of use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
                         </ul>
                     </div>
                     <div className="third-list">
                         <ul>SITES
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
-                            <li>Lorem.</li>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
                         </ul>
                     </div>
 
                 </div>
                 <img src="/img/dc-logo-bg.png" alt="" />
+            </div>
+            <div className="end-footer">
+                <button className="btn">SIGN-UP NOW!</button>
+                <p>FOLLOW US</p>
+                <img src="/img/footer-facebook.png" alt="" />
+                <img src="/img/footer-twitter.png" alt="" />
+                <img src="/img/footer-youtube.png" alt="" />
+                <img src="/img/footer-pinterest.png" alt="" />
+                <img src="/img/periscope.png" alt="" />
             </div>
         </footer>
 
