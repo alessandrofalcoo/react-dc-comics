4951eb1a403c9fcd5362cf5000f91e36f5f707af
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-20 15:39:27 +0100

    add images for comics, logos, and footer; update Header component structure and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,21 @@ export default function Header() {
         <header>
             <img className="logo" src="./img/dc-logo.png" alt="dc header logo" />
             <nav>
-                <ul className="header-list">
-                    <li>CHARACTERS</li>
-                    <li>COMICS</li>
-                    <li>MOVIES</li>
-                    <li>TV</li>
-                    <li>GAMES</li>
-                    <li>COLLECTIBLES</li>
-                    <li>VIDEOS</li>
-                    <li>FANS</li>
-                    <li>NEWS</li>
-                    <li>SHOP</li>
-                </ul>
+                <div>
+                    <ul className="header-list">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+
+                </div>
             </nav>
         </header>
 
