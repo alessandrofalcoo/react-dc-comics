2dbcddd0b373fc50ab487792fd5d4ad16427fdcd
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-20 15:32:46 +0100

    style: update Header component to use uppercase text and add global CSS reset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ export default function Header() {
             <img className="logo" src="./img/dc-logo.png" alt="dc header logo" />
             <nav>
                 <ul className="header-list">
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>Tv</li>
-                    <li>Games</li>
-                    <li>Collectibles</li>
-                    <li>Videos</li>
-                    <li>Fans</li>
-                    <li>News</li>
-                    <li>Shop</li>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
                 </ul>
             </nav>
         </header>
