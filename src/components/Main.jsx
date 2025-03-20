6d7ae8798fc7407d5255208da038719874e04193
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-20 16:28:20 +0100

    style: restructure Main component layout and enhance styling for main menu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,31 @@ export default function Main() {
 
             </div>
             <div className="main-menu">
-                <img src="/img/buy-comics-digital-comics.png" alt="" />
-                <p>DIGITAL COMICS</p>
-                <img src="/img/buy-comics-merchandise.png" alt="" />
-                <p>DC MERCHANDISE</p>
-                <img src="/img/buy-comics-subscription" alt="" />
-                <p>SUBSCRIPTION</p>
-                <img src="/img/buy-comics-shop-locator" alt="" />
-                <p>COMIC SHOP LOCATOR</p>
-                <img src="/img/buy-dc-power-visa.svg" alt="" />
-                <p>DC POWER VISA</p>
+                <div>
+                    <img src="/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+
+                </div>
+                <div>
+                    <img src="/img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+
+                </div>
+                <div>
+
+                    <img src="/img/buy-comics-subscription" alt="" />
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div>
+
+                    <img src="/img/buy-comics-shop-locator" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+                <div>
+
+                    <img src="/img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                </div>
             </div>
         </main>
 
